cb8683913f23f39a7f8eb660a0c4f9db261eab5d
suhasmoshimoshi <suhas09.moshimoshi@gmail.com>
2023-08-21 08:25:20 +0530

    heading chnage

--- components/EducationalContent.js
+++ components/EducationalContent.js
@@ -6,7 +6,7 @@ const EducationalContent = () => {
   return (
     <section className="bg-gray-100 py-16">
       <div className="container mx-auto text-center">
-        <h2 className="text-3xl md:text-4xl font-bold mb-8">
+        <h2 className="text-3xl md:text-4xl font-bold mb-8 px-2">
           Learn About Solar Energy
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
