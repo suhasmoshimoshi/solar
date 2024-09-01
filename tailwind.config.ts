3d3759758f7aff35b256258a9a11271fd7a748f5
suhasmoshimoshi <suhas09.moshimoshi@gmail.com>
2024-09-01 14:43:09 +0530

    chnage

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,8 +14,8 @@ const config: Config = {
       },
       keyframes: {
         fadeInUp: {
-          '0%': { opacity: 0, transform: 'translateY(20px)' },
-          '100%': { opacity: 1, transform: 'translateY(0)' },
+          '0%': { opacity: '0', transform: 'translateY(20px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
         },
       },
       animation: {
